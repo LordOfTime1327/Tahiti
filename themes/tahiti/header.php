4fd9ff8932eb634d59bd64c0a23b5e3275a6edc0
Dmitriy Kaminskiy <dima.vampire98@gmail.com>
2021-10-19 17:26:21 +0300

    header

--- themes/tahiti/header.php
+++ themes/tahiti/header.php
@@ -12,48 +12,38 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+<head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="profile" href="https://gmpg.org/xfn/11">
+
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'tahiti' ); ?></a>
+  <?php wp_body_open(); ?>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$tahiti_description = get_bloginfo( 'description', 'display' );
-			if ( $tahiti_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $tahiti_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+  <header class="header">
+    <div class="container header__row">
+      <div class="header__logo-box">
+        <?= file_get_contents( get_attached_file( get_field( 'logo', 'option' ) ) ); ?>
+      </div>
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'tahiti' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+      <nav class='header__nav header-menu'>
+        <?php 
+					wp_nav_menu([
+						'menu' => 'Header menu',
+						'container' => '',
+						'items_wrap' => '<ul class="header-menu__list">%3$s</ul>'
+					])
+				?>
+      </nav>
+
+      <div class="header__burger">
+        <div class="header__burger-line header__burger-line_first"></div>
+        <div class="header__burger-line header__burger-line_middle"></div>
+        <div class="header__burger-line header__burger-line_last"></div>
+      </div>
+    </div>
+  </header>
